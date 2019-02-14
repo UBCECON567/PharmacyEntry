9b10b0be2321812ac6a0e9e568e4007972c9677a
Paul Schrimpf <paul.schrimpf@gmail.com>
2019-02-13 16:24:07 -0800

    Added notebook for second half.

--- notebooks/make.jl
+++ notebooks/make.jl
@@ -6,8 +6,8 @@ function make(;file="pharmacyentry01-dataprep.jmd", nb = false)
         pandoc_options=["--toc","--toc-depth=2","--filter=pandoc-citeproc"],
         cache=:user)
   if nb
-    notebook(file, :pwd, -1) #, "--allow-errors")
+    notebook(file, :pwd, -1, "--allow-errors")
   end
 end
 make(nb=true)
-
+make(file="pharmacyentry02-model.jmd", nb=true)
